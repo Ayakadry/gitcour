50e1449de0dc16911ace40021f957025ec8881b0
unknown <ayakadry1215@yahoo.com>
2022-02-08 05:07:18 +0200

    created the main project structure

---
+++ js/js.js
@@ -0,0 +1,10 @@
+/*(7>6)?alert("\n\o"):alert("Yes");
+var data =prompt("enter number","0");
+alert(data);
+*/
+var nValue = 3.0;
+var sValue = "3.0";
+if (nValue == sValue)
+document.write("According to equality, TRUE");
+if (nValue === sValue)
+document.write("According to idenity, FALSE");
